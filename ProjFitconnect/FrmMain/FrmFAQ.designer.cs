57b387b62c0262d3b045c1a9460221f270d29511
CHE <celebrate.kirby@gmail.com>
2024-04-02 11:27:58 +0800

    調整已預約課程表單

--- ProjFitconnect/FrmMain/FrmFAQ.designer.cs
+++ ProjFitconnect/FrmMain/FrmFAQ.designer.cs
@@ -43,9 +43,10 @@
             this.label20.BackColor = System.Drawing.Color.LightSlateGray;
             this.label20.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label20.ForeColor = System.Drawing.Color.AliceBlue;
-            this.label20.Location = new System.Drawing.Point(73, 50);
+            this.label20.Location = new System.Drawing.Point(85, 54);
+            this.label20.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label20.Name = "label20";
-            this.label20.Size = new System.Drawing.Size(309, 81);
+            this.label20.Size = new System.Drawing.Size(355, 90);
             this.label20.TabIndex = 54;
             this.label20.Text = "    問題1：\r\n   如何在這個平台上快速找到      \r\n     適合的課程或教練？          ";
             this.label20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -56,9 +57,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(119, 154);
+            this.label1.Location = new System.Drawing.Point(139, 167);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1051, 62);
+            this.label1.Size = new System.Drawing.Size(1181, 70);
             this.label1.TabIndex = 55;
             this.label1.Text = "FitConnect提供了一個搜尋功能，您可以透過平台上的搜尋系統來找到符合您需求的課程或教練。\r\n您還可以查看其他學員的評比和回饋，以幫助您做出更好的選擇。";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -69,9 +71,10 @@
             this.label2.BackColor = System.Drawing.Color.LightSlateGray;
             this.label2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label2.ForeColor = System.Drawing.Color.AliceBlue;
-            this.label2.Location = new System.Drawing.Point(73, 277);
+            this.label2.Location = new System.Drawing.Point(85, 300);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(348, 54);
+            this.label2.Size = new System.Drawing.Size(397, 60);
             this.label2.TabIndex = 56;
             this.label2.Text = "問題2：\r\n我如何知道教練或場地的詳細資訊？";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -82,9 +85,10 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(119, 361);
+            this.label3.Location = new System.Drawing.Point(139, 391);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(926, 62);
+            this.label3.Size = new System.Drawing.Size(1041, 70);
             this.label3.TabIndex = 57;
             this.label3.Text = "FitConnect提供了教練和場地的詳細資訊，包括專業背景、教學經驗、場地設施等。\r\n您可以透過平台上的教練和場地資訊來了解更多相關資訊，以幫助您做出更好的選擇" +
     "。";
@@ -96,9 +100,10 @@
             this.label4.BackColor = System.Drawing.Color.LightSlateGray;
             this.label4.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label4.ForeColor = System.Drawing.Color.AliceBlue;
-            this.label4.Location = new System.Drawing.Point(73, 484);
+            this.label4.Location = new System.Drawing.Point(85, 524);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(285, 54);
+            this.label4.Size = new System.Drawing.Size(325, 60);
             this.label4.TabIndex = 58;
             this.label4.Text = "問題3:\r\n想加入健身房有年齡限制嗎？";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -109,28 +114,31 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(130, 562);
+            this.label5.Location = new System.Drawing.Point(152, 609);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(890, 31);
+            this.label5.Size = new System.Drawing.Size(1002, 35);
             this.label5.TabIndex = 59;
             this.label5.Text = "年滿18歲，可直接加入會員；14歲以上未滿18歲，由法定代理人同意可加入會員。";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // FrmFAQ
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1224, 762);
+            this.ClientSize = new System.Drawing.Size(1428, 826);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label20);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "FrmFAQ";
             this.Text = "FrmQA";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
             this.PerformLayout();
 
